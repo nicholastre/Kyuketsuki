d7111a331e976cabc54cb985f5f8740ca0cac5d7
nicholastre <66094502+nicholastre@users.noreply.github.com>
2021-08-14 22:18:59 -0300

    Criar funções de inventário
    Criar funções de inventário
    Criar função de pegar o item ao clicar com o botão esquerdo do mouse ou itens vinculados ao "Fire1"

--- Kyuketsuki/Assets/Scripts/GameManager.cs
+++ Kyuketsuki/Assets/Scripts/GameManager.cs
@@ -11,12 +11,16 @@ public class GameManager : MonoBehaviour {
 
     public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
 
+    public string[] itemsHeld;
+    public int[] numberOfItems;
+    public Item[] referenceItems;
 
 	// Use this for initialization
 	void Start () {
         instance = this;
 
         DontDestroyOnLoad(gameObject);
+        SortItems();
 
 	}
 	
@@ -30,6 +34,128 @@ public class GameManager : MonoBehaviour {
         else
         {
             PlayerController.instance.canMove = true;
+        }
+        //teste para adicionar ou remover itens
+        /*
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddItem("Dagger");
+            AddItem("teste");
+            RemoveItem("HP Potion");
+            RemoveItem("teste");
+        }*/
+    }
+
+    public Item GetItemDetails(string itemToGrab)
+    {
+        for(int i = 0; i < referenceItems.Length; i++)
+        {
+            if(referenceItems[i].itemName == itemToGrab)
+            {
+                return referenceItems[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void SortItems()
+    {
+        bool itemAfterSpace = true;
+        //listando os itens para que não fiquem jogados espalhados
+        while (itemAfterSpace)
+        {
+            itemAfterSpace = false;
+            for (int i = 0; i < itemsHeld.Length - 1; i++)
+            {
+                if(itemsHeld[i] == "")
+                {
+                    itemsHeld[i] = itemsHeld[i + 1];
+                    itemsHeld[i + 1] = "";
+
+                    numberOfItems[i] = numberOfItems[i + 1];
+                    numberOfItems[i + 1] = 0;
+
+                    if(itemsHeld[i] != "")
+                    {
+                        itemAfterSpace = true;
+                    }
+                }
+            }
+
+        }
+    }
+
+    public void AddItem(string itemToAdd)
+    {
+        int newItemPosition = 0;
+        bool foundSpace = false;
+
+        for( int i = 0; i < itemsHeld.Length; i++)
+        {
+            if(itemsHeld[i] == "" || itemsHeld[i] == itemToAdd)
+            {
+                newItemPosition = i;
+                i = itemsHeld.Length;
+                foundSpace = true;
+            }
+        }
+
+        if (foundSpace)
+        {
+            bool itemExists = false;
+            for(int i = 0; i < referenceItems.Length; i++)
+            {
+                if (referenceItems[i].itemName == itemToAdd)
+                {
+                    itemExists = true;
+
+                    i = referenceItems.Length;
+                }
+            }
+
+            if (itemExists)
+            {
+                itemsHeld[newItemPosition] = itemToAdd;
+                numberOfItems[newItemPosition]++;
+            }
+            else
+            {
+                Debug.LogError(itemToAdd + "Não existe");
+            }
+        }
+
+        GameMenu.instance.ShowItems();
+    }
+
+    public void RemoveItem(string itemToRemove)
+    {
+        bool foundItem =false;
+        int itemPositon = 0;
+
+        for(int i = 0; i < itemsHeld.Length; i++)
+        {
+            if(itemsHeld[i] == itemToRemove)
+            {
+                foundItem = true;
+                itemPositon = i;
+            }
+        }
+
+        if (foundItem)
+        {
+            numberOfItems[itemPositon]--;
+
+            if(numberOfItems[itemPositon] <= 0)
+            {
+                itemsHeld[itemPositon] = "";
+            }
+
+            GameMenu.instance.ShowItems();
+        }
+        else
+        {
+            Debug.LogError("não encontrado" + itemToRemove);
         }
     }
 }
