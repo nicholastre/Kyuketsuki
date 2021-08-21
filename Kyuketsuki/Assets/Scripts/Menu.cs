d22d5622a0d36ad445aa850b60ca62c64cfce622
Silmunia <henriquepcnews@gmail.com>
2021-08-21 20:41:04 -0300

    Implementa resumo de personagens no inventário
    - Implementa resumo de atributos na tela de inventário para cada personagem

--- Kyuketsuki/Assets/Scripts/Menu.cs
+++ Kyuketsuki/Assets/Scripts/Menu.cs
@@ -14,10 +14,16 @@ public enum MenuState
 
 public class Menu : MonoBehaviour
 {
+    // Básico para as telas do menu principal
     public static Menu instance;
     public GameObject menuBackground;
+
+    // Informações da tela principal do menu
     public GameObject[] charDisplays;
     public GameObject[] moneyTrackers;
+
+    // Informações para inventário
+    public GameObject[] summarizedChars;
 
     private MenuState currentState;
 
@@ -77,13 +83,14 @@ public class Menu : MonoBehaviour
                 menuBackground.transform.Find("Inventory").gameObject.SetActive(false);
                 menuBackground.transform.Find("Options").gameObject.SetActive(false);
                 menuBackground.transform.Find("Exit").gameObject.SetActive(false);
-                UpdateCharStats();
+                UpdateCharStats(charDisplays, false);
                 break;
             case MenuState.ItemScreen:
                 menuBackground.transform.Find("Main").gameObject.SetActive(false);
                 menuBackground.transform.Find("Inventory").gameObject.SetActive(true);
                 menuBackground.transform.Find("Options").gameObject.SetActive(false);
                 menuBackground.transform.Find("Exit").gameObject.SetActive(false);
+                UpdateCharStats(summarizedChars, true);
                 break;
             case MenuState.OptionsScreen:
                 menuBackground.transform.Find("Main").gameObject.SetActive(false);
@@ -103,25 +110,30 @@ public class Menu : MonoBehaviour
         }
     }
 
-    private void UpdateCharStats()
+    private void UpdateCharStats(GameObject[] charInfo, bool isSummary)
     {
         CharStats[] storedStats = GameManager.instance.playerStats;
 
         moneyTrackers[0].transform.Find("Value").GetComponent<Text>().text = GameManager.instance.groupMoney.ToString() + " bits";
         moneyTrackers[1].transform.Find("Value").GetComponent<Text>().text = GameManager.instance.groupDebt.ToString() + " bits";
 
-        for (int i = 0; i < charDisplays.Length; i++)
+        for (int i = 0; i < charInfo.Length; i++)
         {
-            charDisplays[i].transform.Find("Char Name").GetComponent<Text>().text = storedStats[i].charName;
-            charDisplays[i].transform.Find("Char Hit Points").GetComponent<Text>().text = "Vida: " + storedStats[i].currentHP + "/" + storedStats[i].maxHP;
-            charDisplays[i].transform.Find("Char Power Points").GetComponent<Text>().text = "Poder: " + storedStats[i].currentMP + "/" + storedStats[i].maxMP;
-            charDisplays[i].transform.Find("Char Level").GetComponent<Text>().text = "Nível " + storedStats[i].playerLevel;
-            charDisplays[i].transform.Find("Char Exp").Find("Slider").GetComponent<Slider>().maxValue = storedStats[i].expToNextLevel[storedStats[i].playerLevel];
-            charDisplays[i].transform.Find("Char Exp").Find("Slider").GetComponent<Slider>().value = storedStats[i].currentEXP;
-            charDisplays[i].transform.Find("Char Exp").Find("Exp To Level").GetComponent<Text>().text = storedStats[i].currentEXP + "/" + storedStats[i].expToNextLevel[storedStats[i].playerLevel];
-            charDisplays[i].transform.Find("Char Str").GetComponent<Text>().text = "Força: " + storedStats[i].strength;
-            charDisplays[i].transform.Find("Char Def").GetComponent<Text>().text = "Defesa: " + storedStats[i].defence;
-            charDisplays[i].transform.Find("Char Agi").GetComponent<Text>().text = "Agilidade: " + storedStats[i].agility;
+
+            charInfo[i].transform.Find("Char Name").GetComponent<Text>().text = storedStats[i].charName;
+            charInfo[i].transform.Find("Char Hit Points").GetComponent<Text>().text = "Vida: " + storedStats[i].currentHP + "/" + storedStats[i].maxHP;
+            charInfo[i].transform.Find("Char Power Points").GetComponent<Text>().text = "Poder: " + storedStats[i].currentMP + "/" + storedStats[i].maxMP;
+            charInfo[i].transform.Find("Char Level").GetComponent<Text>().text = "Nível " + storedStats[i].playerLevel;
+
+            if (!isSummary)
+            {
+                charInfo[i].transform.Find("Char Exp").Find("Slider").GetComponent<Slider>().maxValue = storedStats[i].expToNextLevel[storedStats[i].playerLevel];
+                charInfo[i].transform.Find("Char Exp").Find("Slider").GetComponent<Slider>().value = storedStats[i].currentEXP;
+                charInfo[i].transform.Find("Char Exp").Find("Exp To Level").GetComponent<Text>().text = storedStats[i].currentEXP + "/" + storedStats[i].expToNextLevel[storedStats[i].playerLevel];
+                charInfo[i].transform.Find("Char Str").GetComponent<Text>().text = "Força: " + storedStats[i].strength;
+                charInfo[i].transform.Find("Char Def").GetComponent<Text>().text = "Defesa: " + storedStats[i].defence;
+                charInfo[i].transform.Find("Char Agi").GetComponent<Text>().text = "Agilidade: " + storedStats[i].agility;
+            }
         }
     }
 
